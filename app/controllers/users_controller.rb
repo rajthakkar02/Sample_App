b4fb312602af35f92a1660726af8325e7c2c57ba
rajthakkar02 <rajthakkar0302@gmail.com>
2024-04-12 14:02:24 +0530

    Implemented basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      
     else
       render "new", status: 422
     end
